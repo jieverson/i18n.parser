f546162075b02590356da7a44fd80fc12901997c
Jiéverson Maissiat <jieverson@gmail.com>
2018-11-22 03:49:49 +0000

    no idea what to do with cshtml

--- parser.js
+++ parser.js
@@ -12,21 +12,26 @@ fs.readFile(file_name, 'utf-8', (err, buf) => {
 
     let file = buf.toString()
 
-    file = file.replace(
-        /('[^']*')|("[^']*")/g, 
-        match => {
-            let text = match.slice(1, -1)
-            if( !text
-                || text[0] === '#'
-                || text[0] === '.'
-                || text.includes('/')
-                || text.includes('[[['))
-                return match
-            else{
-                let marker = match[0]
-                return marker + "[[[" + text + "]]]" + marker
-            }
-        })
+    if(ext === 'cshtml'){
+        // Do other stuff...
+    }
+    else {
+        file = file.replace(
+            /('[^']*')|("[^']*")/g, 
+            match => {
+                let text = match.slice(1, -1)
+                if( !text
+                    || text[0] === '#'
+                    || text[0] === '.'
+                    || text.includes('/')
+                    || text.includes('[[['))
+                    return match
+                else{
+                    let marker = match[0]
+                    return marker + "[[[" + text + "]]]" + marker
+                }
+            })
+    }
 
     fs.writeFile(file_name, file, (err, data) => {
         console.log('[Done] ' + file_name)
