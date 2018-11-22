1808eba7082b26c24771a1616519cc675612ae21
Jiéverson Maissiat <jieverson@gmail.com>
2018-11-22 03:30:01 +0000

    first commit

---
+++ parser.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+let file_name = process.argv[2]
+
+fs.readFile(file_name, 'utf-8', (err, buf) => {
+    let ext = file_name.split('.')
+    ext = ext[ext.length - 1]
+    if(ext !== 'js' 
+        && ext !== 'cs' 
+        && ext !== 'cshtml')
+        return;
+
+    let file = buf.toString()
+
+    file = file.replace(
+        /('[^']*')|("[^']*")/g, 
+        match => {
+            let text = match.slice(1, -1)
+            if( !text
+                || text[0] === '#'
+                || text[0] === '.'
+                || text.includes('/')
+                || text.includes('[[['))
+                return match
+            else{
+                let marker = match[0]
+                return marker + "[[[" + text + "]]]" + marker
+            }
+        })
+
+    fs.writeFile(file_name, file, (err, data) => {
+        console.log('[Done] ' + file_name)
+    })
+})
---
+++ test.js
@@ -0,0 +1,41 @@
+﻿function ViewModel() {
+    var viewmodel = this;
+
+    viewmodel.loading = ko.observable(false);
+    viewmodel.modal = $("#idSholdNotChange");
+    
+    $.confirm({
+        title: '',
+        content: 'Essa operação terá efeito nos saldos apresentados no sistema. Alterar saldo inicial?',
+        buttons: {
+            "Cancelar": {},
+            'Alterar': {
+                action: function () {
+                    console.log("bleee");
+                }
+            }
+        }
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: '/Do/Stuff',
+        data: data,
+        success: function (result) {
+            Loader.stop();
+        },
+        error: function (e) {
+            Loader.stop();
+            notificar.erro('Não foi possível do stuff.')
+        }
+    });
+
+
+    viewmodel.moreStuff = function () {
+        viewmodel.inicializarStuff();
+        viewmodel.Welcome.modal({ backdrop: 'static' });
+        viewmodel.Welcome.on('hidden.bs.modal', function () {
+            viewmodel.fuck(null);
+        });
+    }
+}
